b215ab12011ba36e64bda15e8811cd25e65300ad
JuanjoseRojano <tu-email-asociado-a-JuanjoseRojano>
2025-06-03 20:21:28 +0200

    codigoLimpio

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -1,14 +1,13 @@
-// https://www.mongodb.com/developer/languages/javascript/integrate-mongodb-vercel-functions-serverless-experience/
-import { MongoClient } from "mongodb";
+import { MongoClient } from "mongodb"
 
-const uri = process.env.MONGODB_URI; //Aqui la conexion
+const uri = process.env.MONGODB_URI
 const options = {
     // useUnifiedTopology: true,
     // useNewUrlParser: true,
-};
+}
 
-let mongoClient = null;
-let database = null;
+let mongoClient = null
+let database = null
 
 if (!process.env.MONGODB_URI) {
     throw new Error('Please add your Mongo URI to .env.local')
@@ -17,22 +16,22 @@ if (!process.env.MONGODB_URI) {
 export async function connectToDatabase() {
     try {
         if (mongoClient && database) {
-            return { mongoClient, database };
+            return { mongoClient, database }
         }
         if (process.env.NODE_ENV === "development") {
             if (!global._mongoClient) {
-                mongoClient = await (new MongoClient(uri, options)).connect();
-                global._mongoClient = mongoClient;
+                mongoClient = await (new MongoClient(uri, options)).connect()
+                global._mongoClient = mongoClient
             } else {
-                mongoClient = global._mongoClient;
+                mongoClient = global._mongoClient
             }
         } else {
-            mongoClient = await (new MongoClient(uri, options)).connect();
+            mongoClient = await (new MongoClient(uri, options)).connect()
         }
-        database = await mongoClient.db(process.env.MONGODB_DATABASE);
-        return { mongoClient, database };
+        database = await mongoClient.db(process.env.MONGODB_DATABASE)
+        return { mongoClient, database }
     } catch (e) {
-        console.error(e);
+        console.error(e)
     }
 }
 
